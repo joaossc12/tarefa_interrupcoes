579f39064ed90bb41fd631ce3b483b6088592d11
João Vitor <joaossc.ege@gmail.com>
2025-01-28 23:29:46 -0300

    Adicionando o circuito e configurando pinos

--- tarefa_interrupcoes.c
+++ tarefa_interrupcoes.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define led_red 13
+#define led_blue 12
+#define led_green 11
+#define led_matrix 7
+#define button_a 5
+#define button_b 6
 
+void init_pinos();
 
 int main()
-{
+{   
+    init_pinos();
     stdio_init_all();
 
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+}
+
+void init_pinos(){
+  gpio_init(led_red);//inicializa o pino 13 do microcontrolador
+  gpio_set_dir(led_red, GPIO_OUT);//configura o pino como saída
+ 
+  gpio_init(led_blue);//inicializa o pino 12 do microcontrolador 
+  gpio_set_dir(led_blue, GPIO_OUT);//configura o pino 12 como saída
+ 
+  gpio_init(led_green); //inicializa o pino 11 do microcontrolador
+  gpio_set_dir(led_green,GPIO_OUT); //configura o pino 11 como saída
+
+  gpio_init(button_a);//inicializa o pino 5 do microcontrolador
+  gpio_set_dir(button_a, GPIO_IN);//configura o pino como entrada
+  gpio_pull_up(button_a); //Pull up pino 5
+
+  gpio_init(button_b);//inicializa o pino 6 do microcontrolador
+  gpio_set_dir(button_b, GPIO_IN);//configura o pino como entrada
+  gpio_pull_up(button_b); //Pull up pino 6
 }
